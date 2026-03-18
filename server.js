d8ec98cf8062674c6814aa6fea4ffc7732a07c7d
Ahmad <ahmadisaev6878@gmail.com>
2026-03-18 03:05:41 +0300

    update for railway

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ function broadcast(data) {
   });
 }
 
-server.listen(3000, () => {
-  console.log('Сервер запущен на http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
 });
